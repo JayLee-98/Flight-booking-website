95ef08c93c95b6d0dcd90444a43be5a0a99867cf
jaylee <jayginwoolee98@gmail.com>
2024-06-21 23:38:37 +0900

    git init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Home, Info, Lounge, Navbar, Search, Subscribe, Support, Travelers, Footer } from './Components';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* <Home />
+      <Search />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travelers />
+      <Subscribe />
+      <Footer />   */}    
+    </div>
+  )
+}
+
+export default App;
