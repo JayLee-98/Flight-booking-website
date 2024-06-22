144c3dd5d0657c95a71c42f95ad8747a27efa20c
jaylee <jayginwoolee98@gmail.com>
2024-06-22 23:33:14 +0900

    info styling done

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
+import { BsBookmarkCheck, BsShieldCheck } from "react-icons/bs";
+import { RxCalendar } from "react-icons/rx";
 
 const Info = () => {
-    return (
-        <div>
-            Info
+  return (
+    <div className="info section">
+      <div className="infoContainer container">
+        <div className="titleDiv flex">
+          <h2>Travel to make memories all around the world</h2>
+          <button className="btn">View All</button>
         </div>
-    )
-}
+
+        <div className="cardsDiv  ">
+
+          <div className="singleCard grid">
+            <div className="iconDiv flex">
+              <RxCalendar className="icon" />
+            </div>
+            <span className="cardTitle">Book & Relax</span>
+            <p>You can also airlines from your phone and book a flight ticket!</p>
+          </div>
+
+          <div className="singleCard grid">
+            <div className="iconDiv flex colorOne">
+              <BsShieldCheck className="icon" />
+            </div>
+            <span className="cardTitle">Smart Checklist</span>
+            <p>You can also airlines from your phone and book a flight ticket!</p>
+          </div>      
+
+          <div className="singleCard grid">
+            <div className="iconDiv flex colorTwo">
+              <BsBookmarkCheck className="icon" />
+            </div>
+            <span className="cardTitle">Save More</span>
+            <p>You can also airlines from your phone and book a flight ticket!</p>
+          </div>    
+
+          </div>
+      </div>
+    </div>
+  );
+};
 
 export default Info;
